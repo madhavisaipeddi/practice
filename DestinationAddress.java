132254eff330831622ebd80eec6a4cd826eed20b
madhavisaipeddi <madhavisai28@gmail.com>
2021-03-13 13:01:01 +0530

    First commit

---
+++ DestinationAddress.java
@@ -0,0 +1,11 @@
+public class DestinationAddress
+{
+ 
+   String line1 ="muyhyala" ;
+  String line2="reddy 4th lane"; 
+   String city ="guntur";
+  String state ="andhra pradesh";
+   String province= "indian";
+   String countryCode="ap";
+   String zipCode ="indian533";
+}
