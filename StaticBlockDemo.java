058ff6745e689d1930c9b31ed0190dc0b1fdc11b
madhavisaipeddi <madhavisai28@gmail.com>
2021-03-13 13:30:41 +0530

    second commit

---
+++ StaticBlockDemo.java
@@ -0,0 +1,9 @@
+public class StaticBlockDemo{
+ public static void main(String args[])
+{
+ StaticBlock st=new StaticBlock();
+
+System.out.println("========================");
+  StaticBlock st1=new StaticBlock();
+}
+}
