994b747d7da07c531f1cbe06e63dd17603129985
madhavisaipeddi <madhavisai28@gmail.com>
2021-03-21 12:09:37 +0530

    Datatypes

---
+++ ShippingSpeedController.java
@@ -0,0 +1,21 @@
+public class ShippingSpeedController
+{
+ public static void main(String args[])
+{
+ System.out.println("main method stars");
+ShippingSpeed s1=new ShippingSpeed();
+System.out.println(s1.shippingSpeedCode);
+System.out.println(s1.shippingSpeedName);
+System.out.println(s1.shippingSpeedDesc);
+System.out.println(s1.shippingTypeId);
+
+
+ System.out.println("main second  method stars===");
+ShippingSpeed s2=new ShippingSpeed();
+System.out.println(s2.shippingSpeedCode);
+System.out.println(s2.shippingSpeedName);
+System.out.println(s2.shippingSpeedDesc);
+System.out.println(s2.shippingTypeId);
+ System.out.println("main second  method ends===");
+}
+}
